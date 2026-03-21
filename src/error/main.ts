1093724f94f9be3d4a9f64e97119f90bf14dcfac
Niklas M. <contact@shinaii.xyz>
2026-03-22 00:17:00 +0100

    feat: Integrate Font Awesome icons and update components for improved UI

--- src/error/main.ts
+++ src/error/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import '@/portal/style.css'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faDiscord } from '@fortawesome/free-brands-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faDiscord)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
